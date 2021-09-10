697c922c83970f30a1f48d6cc62202d9d419b8a6
lqt2k <wintrung01@gmail.com>
2021-09-11 00:25:01 +0700

    Initial commit

---
+++ DatPhongKS/GUI/DangNhap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public enum DangNhap
+    {
+        Invalid, //Kiểm tra
+        Disable,//Vô hiệu hoá
+        Success //Thành công
+    }
+}
---
+++ DatPhongKS/GUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    static class Program
+    {
+        public static FrmDangNhap frmDangNhap = null;
+        public static FrmMain frmMain = null;
+        public static FrmDatPhong frmDatPhong = null;
+        public static FrmTraPhong frmTraPhong = null;
+        public static FrmCauHinh frmCauHinh = null;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            frmDangNhap = new FrmDangNhap();
+            //frmCauHinh = new FrmCauHinh();
+            Application.Run(frmDangNhap);
+        }
+    }
+}
